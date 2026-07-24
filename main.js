6787cfb73b8c3cb910e6e980eaa397e5631210ec
nardek48 <vyghjyttrj@privaterelay.appleid.com>
2026-07-24 17:43:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 "use strict";
+alert("main.js chargé")
 /* ============================================================
 Quest Idle — main.js (version refaite)
 Combat, achats, talents, ascension, boucle de jeu, initialisation
