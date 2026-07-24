f8ff87f9a37d66f7cea2345d7133cb74912ef10f
nardek48 <vyghjyttrj@privaterelay.appleid.com>
2026-07-24 17:44:31 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,4 @@
 "use strict";
-alert("main.js chargé")
 /* ============================================================
    QUEST IDLE — data.js
    Données statiques du jeu : assets, mondes, upgrades, talents,
