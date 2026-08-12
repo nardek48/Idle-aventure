14cafd0c22155e185d26433ed6ce08541523bfbf
nardek48 <vyghjyttrj@privaterelay.appleid.com>
2026-08-12 09:18:55 +0200

    v2.90.15

--- sw.js
+++ sw.js
@@ -45,7 +45,7 @@ les onglets ouverts via postMessage (voir la fin de l'event
 petite bannière "Nouvelle version disponible — Recharger".
 ============================================================ */
 
-var CACHE_VERSION = "2.90.11"; // <- à incrémenter à CHAQUE livraison
+var CACHE_VERSION = "2.90.15"; // <- à incrémenter à CHAQUE livraison
 var CACHE_NAME = "quest-idle-" + CACHE_VERSION;
 
 var PRECACHE_APP_SHELL = [
